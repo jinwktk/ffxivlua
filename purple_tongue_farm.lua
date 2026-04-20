0f3d9dc702b02d84f7fe6ea0e9760dbfebde80e7
jinwktk <mlove.rlove.klove@gmail.com>
2026-04-20 18:46:30 +0900

    fix: abort when inventory full with 0 fish (nothing to reduce, cannot fish more)

--- purple_tongue_farm.lua
+++ purple_tongue_farm.lua
@@ -74,8 +74,8 @@ configs:
 ------------------------------------------------------------------
 -- バージョン識別 (git pre-commit hook で自動置換される) ---------
 ------------------------------------------------------------------
-local SCRIPT_VERSION = "a1d7ce8"                -- AUTO-UPDATED BY HOOK
-local SCRIPT_BUILD   = "2026-04-20 18:44"                -- AUTO-UPDATED BY HOOK
+local SCRIPT_VERSION = "81612d3"                -- AUTO-UPDATED BY HOOK
+local SCRIPT_BUILD   = "2026-04-20 18:46"                -- AUTO-UPDATED BY HOOK
 
 ------------------------------------------------------------------
 -- Config 読み込み ----------------------------------------------
